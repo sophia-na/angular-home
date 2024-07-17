afd3da27651ef3c3638d8bcba44b7c8db4e4a14b
shonamz <shokoufeh.namazian@gmail.com>
2024-07-17 10:00:34 -0700

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,HomeComponent],
+  // templateUrl: './app.component.html',
+  template:`
+  <main>
+    <header class="brand-name">
+      <!-- <img class="brand-logo" > -->
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ang-test';
+  title = 'homes';
 }
