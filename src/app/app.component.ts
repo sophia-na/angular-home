2a3f32ee493d635fbb798aa0b9bc9f3ff25c3654
shonamz <shokoufeh.namazian@gmail.com>
2024-07-22 11:26:31 -0700

    11 integrate details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { RouterModule } from '@angular/router';
-
+ 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent,RouterModule],
+  imports: [RouterOutlet,HomeComponent,RouterLink],
   // templateUrl: './app.component.html',
   template:`
-  <main>
+   <main>
     <a [routerLink]="['/']">
       <header class="brand-name">
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation';
+import{ RouterLink,RouterModule,RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterModule],
   template: `
   <section class="listing">
     <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
     <h2 class="listing-heading">{{ housingLocation.name }}</h2>
     <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
   </section>
   `,
   styleUrl: './housing-location.component.css'
@@ -18,3 +20,4 @@ import { HousingLocation } from '../housinglocation';
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
 }
+ 
