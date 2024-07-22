1a3d4a1d740db38b8bc255ee931bd6f39572b7b7
shonamz <shokoufeh.namazian@gmail.com>
2024-07-22 13:42:09 -0700

    12 add form

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -124,5 +124,6 @@ export class HousingService {
   submitApplication(firstName: string, lastName: string, email: string) {
     console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
   }
+  
 
 }
